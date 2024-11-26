b19cfdbae9e747f3bab1a0d64a7a3b0d5ae73108
mickoymouse <mckslvstr@gmail.com>
2024-11-26 17:57:17 +0800

    1. Added custom colors. 2. Mocked up UI.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,5 +11,33 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-	return <div>Advice # 1</div>;
+	return (
+		<main className="bg-dark-blue h-screen w-screen">
+			<section className="h-full w-full flex items-center justify-center px-4">
+				<div
+					className="bg-dark-grayish-blue w-full min-h-[315px] max-h-[315px] rounded-lg 
+                      flex flex-col items-center justify-center px-4 gap-6 relative
+                      md:max-w-[540px] md:min-h-[332px] md:max-h-[332px] md:px-16"
+				>
+					<h1 className="text-[11px] font-semibold text-neon-green tracking-[3.46px] uppercase md:text-[13px]">
+						Advice #117
+					</h1>
+					<p className="text-center text-2xl font-semibold text-white tracking-[-0.26px] md:text-[28px]">
+						"It is easy to sit up and take notice, what's difficult is getting
+						up and taking action."
+					</p>
+					<picture className="pb-6">
+						<source
+							media="(min-width: 768px)"
+							srcSet="/pattern-divider-desktop.svg"
+						/>
+						<img src="/pattern-divider-mobile.svg" alt="divider" />
+					</picture>
+					<div className="w-[64px] h-[64px] bg-neon-green rounded-full flex items-center justify-center absolute bottom-[-32px] gradient-shadow cursor-pointer">
+						<img src="/icon-dice.svg" alt="dice" />
+					</div>
+				</div>
+			</section>
+		</main>
+	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,17 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          "Inter",
-          "ui-sans-serif",
-          "system-ui",
-          "sans-serif",
-          "Apple Color Emoji",
-          "Segoe UI Emoji",
-          "Segoe UI Symbol",
-          "Noto Color Emoji",
-        ],
-      },
-    },
-  },
-  plugins: [],
+	content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"dark-blue": "hsl(219, 22, 16)",
+				"dark-grayish-blue": "hsl(217, 19, 24)",
+				"grayish-blue": "hsl(217, 19, 38)",
+				"light-cyan": "hsl(193, 38. 86)",
+				"neon-green": "hsl(150, 100, 66)",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
