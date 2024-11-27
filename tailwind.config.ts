2e61911ab05c37689d7bd07aad02762fd87f37d6
mickoymouse <mckslvstr@gmail.com>
2024-11-27 16:48:00 +0800

    Fixed config for color: light-cyan.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ export default {
 				"dark-blue": "hsl(219, 22, 16)",
 				"dark-grayish-blue": "hsl(217, 19, 24)",
 				"grayish-blue": "hsl(217, 19, 38)",
-				"light-cyan": "hsl(193, 38. 86)",
+				"light-cyan": "hsl(193, 38, 86)",
 				"neon-green": "hsl(150, 100, 66)",
 			},
 		},
